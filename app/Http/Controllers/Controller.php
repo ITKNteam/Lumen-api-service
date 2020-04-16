e2bf256f81036d87c9c2804e9fd8e0a8243c6621
Andrey <fosteev3@gmail.com>
2020-04-16 15:19:27 +0300

    Add method hasRequest

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,19 @@ namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
 
-class Controller extends BaseController
-{
-    //
+class Controller extends BaseController {
+    /**
+     * Check params in request
+     * @param Request $request
+     * @param $key
+     */
+    public function requestHas(Request $request, $key) {
+        if (!$request->has($key)) {
+            if (is_array($key)) {
+                abort(400, "Missing parameter one of: " . implode(',', $key));
+            } else {
+                abort(400, "Missing parameter: $key");
+            }
+        }
+    }
 }
